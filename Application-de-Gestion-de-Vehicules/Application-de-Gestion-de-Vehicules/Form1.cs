153a028c2e284e1bba0166c61894a0d9ca2cb659
HAMZA OUBAHIM <33463744+hamzaoub@users.noreply.github.com>
2025-04-03 00:26:22 -0400

    base code

---
+++ Application-de-Gestion-de-Vehicules/Application-de-Gestion-de-Vehicules/Form1.cs
@@ -0,0 +1,10 @@
+namespace Application_de_Gestion_de_Vehicules
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
